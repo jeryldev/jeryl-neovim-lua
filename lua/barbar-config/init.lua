f061b8eedf262715504100c215a4260d9c2e87d3
jeryldev <jeryldev@gmail.com>
2022-12-04 23:22:59 +0800

    created neovim lua setup

---
+++ lua/barbar-config/init.lua
@@ -0,0 +1,6 @@
+vim.g.bufferLine = {
+    animation = true, 
+    closable = true,
+    icon_separator_active = '',
+    icon_separator_inactive = '',
+}
---
+++ lua/packer-config/init.lua
@@ -0,0 +1,39 @@
+return require('packer').startup(function()
+    use 'wbthomason/packer.nvim'
+    use 'nvim-tree/nvim-web-devicons'
+    use 'EdenEast/nightfox.nvim'
+    use 'nvim-tree/nvim-tree.lua'
+
+    use 'rcarriga/nvim-notify'
+    use 'nvim-lualine/lualine.nvim'
+    use 'romgrk/barbar.nvim'
+
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use 'nvim-treesitter/nvim-treesitter-refactor'
+    
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip' -- Snippets plugin
+    use 'onsails/lspkind-nvim'
+
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+
+    -- you need a snippet engine for snippet support
+    -- here I'm using vsnip which can load snippets in vscode format
+    use 'hrsh7th/vim-vsnip'
+    use 'hrsh7th/cmp-vsnip'
+end)
