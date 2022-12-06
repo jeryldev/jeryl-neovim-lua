5dfb90e0ab8345b8a3bb6896fd2df272c27d362f
jeryldev <jeryldev@gmail.com>
2022-12-06 08:43:51 +0800

    updated spacing to test lazygit

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -21,14 +21,12 @@ return require('packer').startup(function(use)
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
-
     use {
         'lewis6991/gitsigns.nvim',
         config = function()
             require('gitsigns').setup()
         end
     }
-
     use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
     use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
     use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
